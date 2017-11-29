eb513c52f71ffcb2b3dbfb0eff605149733277bb
Alafran <33580422+Alafran@users.noreply.github.com>
2017-11-29 18:42:23 -0500

    Add files via upload

---
+++ FileManager.java
@@ -0,0 +1,38 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileManager {
+
+    public List<String> read(String file) throws FileNotFoundException {
+        File fileToBeRead = new File(file);
+        Scanner reader = new Scanner(fileToBeRead);
+        
+        List<String> linesInFile = new ArrayList<String>();
+        
+        while (reader.hasNext()) {
+            linesInFile.add(reader.nextLine());
+        }
+        
+        return linesInFile;
+    }
+
+    public void save(String file, String text) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        writer.write(text);
+        writer.close();
+    }
+
+    public void save(String file, List<String> texts) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        for(String text : texts) {
+            writer.write(text +"\n");
+        }
+        writer.close();
+    }
+}
